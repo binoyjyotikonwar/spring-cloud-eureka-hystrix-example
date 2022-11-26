cf4168131d4a35d78f560a3dbc90d88bbe49d4ca
‘binoyjyotikonwar‘ <binoybjk@gmail.com>
2022-11-27 01:39:12 +0800

    initial commit

---
+++ hello-client/src/test/java/org/binoy/konwar/helloclient/HelloClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.binoy.konwar.helloclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ hello-server/src/test/java/org/binoy/konwar/helloserver/HelloServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.binoy.konwar.helloserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
